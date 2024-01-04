85d0ea9cae596c58cfe94804e79e0f2b58f4589d
dd84ai <dd84ai@gmail.com>
2024-01-04 23:12:58 +0100

    test: fix two of them

--- fldarklint/cmd/cmd_utils/rand_line/main_test.go
+++ fldarklint/cmd/cmd_utils/rand_line/main_test.go
@@ -1,10 +1,15 @@
 package rand_line
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/darklab8/darklab_goutils/goutils/utils"
 )
 
 func TestSimple(t *testing.T) {
+	os.MkdirAll(filepath.Join(string(utils.GetCurrentFolder()), "tests", "temp"), 0777)
 	times := 5
 	input := Input{
 		InputFilePath:  "tests/data/input.txt",
--- fldarklint/cmd/cmd_utils/rand_line_test.go
+++ fldarklint/cmd/cmd_utils/rand_line_test.go
@@ -2,12 +2,20 @@ package cmd_utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	"darklint/fldarklint/settings/logus"
+
+	"github.com/darklab8/darklab_goutils/goutils/utils"
 )
 
 func TestRandLine(t *testing.T) {
+	err := os.MkdirAll(filepath.Join(string(utils.GetCurrentFolder()), "rand_line", "tests", "temp"), 0777)
+	logus.Log.CheckError(err, "failed to create folder")
 	arguments := strings.Split("run . utils rand_line --input fldarklint/cmd/cmd_utils/rand_line/tests/data/input.txt --output fldarklint/cmd/cmd_utils/rand_line/tests/temp/output.txt -k 5", " ")
 	cmd := exec.Command("go", arguments...)
 	cmd.Dir = "../../.."
